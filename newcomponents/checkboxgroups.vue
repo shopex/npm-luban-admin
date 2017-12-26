8a41fac75b28db8acdb7ebb323b972bd9ef2e716
lamack <shopex@administratordeMac-mini-2.local>
2017-12-26 15:23:00 +0800

    check groups

--- newcomponents/checkboxgroups.vue
+++ newcomponents/checkboxgroups.vue
@@ -3,14 +3,14 @@
 		<div v-for="(item,key) in choiceGroup">
 			选择产品大类&nbsp;&nbsp;{{item.content}}（共<span class="num">{{item.children.length}}</span>项目，已选<span class="num">{{item.choice.length}}</span>项目）
 			<el-checkbox :indeterminate="item.isIndeterminate" v-model="item.checkAll" @change="handleCheckAllChange(key)">全选</el-checkbox>
-			<el-checkbox-group style="padding-left:30px;" v-model="item.choice" @change="handleCheckedCitiesChange(key)">
+			<el-checkbox-group class="group" v-model="item.choice" @change="handleCheckedCitiesChange(key)">
 				<el-checkbox v-for="(child,i) in item.children" :label="child" :key="child.id" @change="checkedChange(key,i,$event)">{{child.content}}</el-checkbox>
 			</el-checkbox-group>
 
 			<div v-if="item.children[i].children" v-for="(child,i) in item.children" style="padding-left:30px;" >
 				选择产品小类&nbsp;&nbsp;{{child.content}}（共<span class="num">{{child.children.length}}</span>项目，已选<span class="num">{{child.choice.length}}</span>项目）
 				<el-checkbox :indeterminate="child.isIndeterminate" v-model="child.checkAll" @change="changeall(key,i)">全选</el-checkbox>
-				<el-checkbox-group style="padding-left:30px;" v-model="child.choice" @change="handleChange(key,i)">
+				<el-checkbox-group class="group" style="padding-left:30px;" v-model="child.choice" @change="handleChange(key,i)">
 					<el-checkbox v-for="(grandchild,i) in child.children" :label="grandchild" :key="grandchild.id">{{grandchild.content}}</el-checkbox>
 				</el-checkbox-group>
 			</div>
@@ -19,14 +19,23 @@
 </template>
 <style lang="scss" scoped>
 	.num{color:green;}
+	.group{
+	}
+	.group >>> label:first-child{
+			margin-left:30px;
+		}
 </style>
 <script type="text/javascript">
 	export default{
-		prop:['options'],
+		props:['options','url'],
 		data(){
 			return{
 				choiceGroup:this.options,
 			}
+		},
+		mounted(){
+			var that = this;
+			console.log(this.url,99)
 		},
 		methods: {
 		    handleCheckAllChange(val) {
@@ -74,7 +83,7 @@
 		    	var id = data.id;
 		    	$.ajax({
 		    	  'type':'get',
-		    	  'url':"{{url('basedata_producttree/getSmallCate')}}",
+		    	  'url':that.url,
 		    	  'data':{
 		    	  	id:id,
 		    	  },
